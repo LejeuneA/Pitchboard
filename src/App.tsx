a9533ad1fe3e2d69fd448246110d1c4160c551d8
A.Lejeune <acelyalejeune@gmail.com>
2026-09-24 15:45:03 +0200

    Render freelance records from typed data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { freelanceRecords } from './data/freelanceRecords'
+
 function App() {
 
   return (
     <>
       <h1>Pitchboard</h1>
+
+      {freelanceRecords.map((record) =>
+        <div key={record.id}>
+          <p>Name: {record.name}</p>
+          <p>Status: {record.status}</p>
+        </div>)}
     </>
   )
 }
