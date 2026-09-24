681278f203b57333f078ae8bfd3eddf33cb35027
A.Lejeune <acelyalejeune@gmail.com>
2026-09-24 15:13:22 +0200

    Fix FreelanceRecord fields and union types

--- src/types/FreelanceRecord.ts
+++ src/types/FreelanceRecord.ts
@@ -1,8 +1,9 @@
 export type FreelanceRecord = {
   id: number
+  date: string
   name: string
   location?: string
-  entityType: 'Company | Person'
-  recordCategory: 'Application | Partner Lead'
+  entityType: 'Company' | 'Person'
+  recordCategory: 'Application' | 'Partner Lead'
   status: string
 }
