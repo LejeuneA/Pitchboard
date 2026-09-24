ae22c8b86a5cc49ba336e04308a7062c92d291cd
A.Lejeune <acelyalejeune@gmail.com>
2026-09-24 15:29:58 +0200

    Fix first freelance record data

--- src/data/freelanceRecords.ts
+++ src/data/freelanceRecords.ts
@@ -3,9 +3,9 @@ import type { FreelanceRecord } from '../types/FreelanceRecord'
 
 const firstRecord: FreelanceRecord = {
   id: 1,
-  date: '10.10.2026',
+  date: '10.09.2026',
   name: 'Story Space',
-  location?: 'United Kingdom',
+  location: 'United Kingdom',
   entityType: 'Company',
   recordCategory: 'Application',
   status: 'Sent- Waiting for answer',
