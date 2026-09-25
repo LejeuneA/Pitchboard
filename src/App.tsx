12ffa7b83693784d13358ae18e431010cde62954
A.Lejeune <acelyalejeune@gmail.com>
2026-09-25 13:44:26 +0200

    Complete delete handler logic

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
   const [records, setRecords] = useState(freelanceRecords)
 
   function handleDelete(id: number) {
-    const newRecords = records.filter(record.id !== 10)
-    setRecords((newRecords))
+    const newRecords = records.filter((record) => record.id !== id)
+    setRecords(newRecords)
   }
 
   return (
