f2cdda87e054148ebc432ea3b48ab9a6b2967157
A.Lejeune <acelyalejeune@gmail.com>
2026-09-25 09:03:35 +0200

    Add second freelance record

--- src/components/FreelanceRecordCard.tsx
+++ src/components/FreelanceRecordCard.tsx
@@ -3,8 +3,12 @@ import type { FreelanceRecordCardProps } from '../types/FreelanceRecord'
 function FreelanceRecordCard(props: FreelanceRecordCardProps) {
   return (
     <div>
+      <p>Date: {props.record.date}</p>
       <p>Name: {props.record.name}</p>
+      <p>Name: {props.record.location}</p>
       <p>Status: {props.record.status}</p>
+      <p>{props.record.entityType = 'Company'}</p>
+      <p>{props.record.recordCategory = 'Application'}</p>
     </div>
   )
 }
--- src/data/freelanceRecords.ts
+++ src/data/freelanceRecords.ts
@@ -10,5 +10,193 @@ export const freelanceRecords: FreelanceRecord[] = [
     entityType: 'Company',
     recordCategory: 'Application',
     status: 'Sent- Waiting for answer',
-  }
+  },
+
+  {
+    id: 2,
+    date: '14.09.2026',
+    name: 'Creative Circus',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 3,
+    date: '15.09.2026',
+    name: 'White Label Solutions',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 4,
+    date: '16.09.2026',
+    name: 'Wonderland',
+    location: 'Amsterdam',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 5,
+    date: '16.09.2026',
+    name: 'Jacobs Development',
+    location: 'The Hogue',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 6,
+    date: '16.09.2026',
+    name: 'Aixiocom',
+    location: 'Belgium',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 7,
+    date: '17.09.2026',
+    name: 'Pony Studio',
+    location: 'London',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 8,
+    date: '17.09.2026',
+    name: 'Michiel Van Alphen',
+    location: 'Netherlands',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 9,
+    date: '18.09.2026',
+    name: 'Dept',
+    location: 'Netherlands',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 10,
+    date: '18.09.2026',
+    name: 'Nathan Pichon',
+    location: 'France',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 11,
+    date: '21.09.2026',
+    name: 'Apoomatic',
+    location: 'Sweden',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 12,
+    date: '21.09.2026',
+    name: 'Bewort',
+    location: 'Remote Europe',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 13,
+    date: '21.09.2026',
+    name: 'Roman Talboom',
+    location: 'Amsterdam',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 14,
+    date: '22.09.2026',
+    name: 'Zoltan Biro',
+    location: 'Budapest',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 15,
+    date: '23.09.2026',
+    name: 'Alexandre Petitjean',
+    location: 'Lyon/France',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 16,
+    date: '23.09.2026',
+    name: 'Epic Agency',
+    location: 'Belgium',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 17,
+    date: '24.09.2026',
+    name: 'Ubernu',
+    location: 'Germany',
+    entityType: 'Company',
+    recordCategory: 'Application',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 18,
+    date: '24.09.2026',
+    name: 'Joad Neto',
+    location: 'Porto/Portugal',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 19,
+    date: '25.09.2026',
+    name: 'Arno Van Oordt',
+    location: 'Netherlands',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
+
+  {
+    id: 20,
+    date: '25.09.2026',
+    name: 'Krill Strelnicon',
+    location: 'Barcelona/Spain',
+    entityType: 'Person',
+    recordCategory: 'Partner Lead',
+    status: 'Sent- Waiting for answer',
+  },
 ]
